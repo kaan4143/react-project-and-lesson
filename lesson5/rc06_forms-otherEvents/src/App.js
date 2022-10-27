bf5b75f958adffc2e3f57e546be51817fafff27e
tuğrul kaan kılıç <tkaankilic@gmail.com>
2022-10-27 17:36:58 +0300

    a

--- lesson5/rc06_forms-otherEvents/src/App.js
+++ lesson5/rc06_forms-otherEvents/src/App.js
@@ -1,5 +1,5 @@
 // import Form from "./components/1-forms/Form";
-// import FormObject from "./components/1-forms/FormObject";
+// import FormObjesct from "./components/1-forms/FormObject";
 // import KeybboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
 import MouseEvent from "./components/3-mouseEvents/MouseEvent";
 
